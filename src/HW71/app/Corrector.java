100667863f8c82100d6d55ff260121202273fbd8
Valentyn_Koval <J.K.play2015@gmail.com>
2026-01-13 20:53:02 +0200

    change method from static to non-static

--- src/HW71/app/Corrector.java
+++ src/HW71/app/Corrector.java
@@ -1,7 +1,7 @@
 package HW71.app;
 
 public class Corrector {
-    public static String handleData(String[] strs) {
+    public String handleData(String[] strs) {
         StringBuilder stringBuilder = new StringBuilder();
         int count = 0;
         for (String str : strs) {
