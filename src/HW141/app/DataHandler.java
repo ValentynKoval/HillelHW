731af6ab957377d9e720b90cd5e52aa8feba0eee
Valentyn_Koval <J.K.play2015@gmail.com>
2026-02-23 20:42:57 +0200

    complete homework 14.1

--- src/HW141/app/DataHandler.java
+++ src/HW141/app/DataHandler.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class DataHandler {
-    // Метод формує виведення імені за певним індексом
     public String formOutput(List<String> list, int index) {
         try {
             String name = list.get(index);
@@ -14,7 +13,6 @@ public class DataHandler {
         }
     }
 
-    // Метод формує виведення нумерованого списку імен
     public String formListOutput(List<String> list) {
         StringBuilder sb = new StringBuilder();
         AtomicInteger count = new AtomicInteger(1);
