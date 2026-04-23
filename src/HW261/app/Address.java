e54cb2e9208c0ddef8117b7953a2f8a74e45786f
Valentyn_Koval <J.K.play2015@gmail.com>
2026-04-23 14:51:08 +0300

    complete homework 26.1

--- src/HW261/app/Address.java
+++ src/HW261/app/Address.java
@@ -11,30 +11,6 @@ public class Address {
         this.houseNumber = houseNumber;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
     @Override
     public String toString() {
         return "м. " + city + ", вул. " + street + ", буд. " + houseNumber;
